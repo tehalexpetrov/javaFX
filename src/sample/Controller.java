06d28f442b251e89f32cd368d210225b06fce6da
Alexander <tehalexpetrov@gmail.com>
2020-12-21 01:16:44 +0300

    javaFX начал писать код

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,4 +1,27 @@
 package sample;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.*;
+import java.awt.event.*;
+import java.util.Random;
+
 public class Controller {
+
+    @FXML
+    public void  initialize(){
+
+    }
+
+    @FXML
+    private Label answerLabel;
+
+    @FXML
+    private TextField userAnswer;
+
+    @FXML
+    void doUserAction(ActionEvent event) {
+        Random randome = new Random();
+        String message =
+    }
+
 }
