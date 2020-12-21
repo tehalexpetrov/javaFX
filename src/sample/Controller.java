2d5739c717811a59274d6ecf2e756dd9b7176531
Alexander <tehalexpetrov@gmail.com>
2020-12-21 10:06:46 +0300

    цикл for работает не правильно

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -2,13 +2,17 @@ package sample;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.*;
-import java.awt.event.*;
-import java.util.Random;
+import java.util.*;
 
 public class Controller {
 
-    @FXML
-    public void  initialize(){
+    public int maxCount = 3;
+    public int count = 0;
+    public int multiple = (int) Math.round(Math.random() * 9);
+    private String winer = "Поздравляю! Вы угадали число";
+    private String messageCount = "У Вас осталось ";
+
+    public void initialize(){
 
     }
 
@@ -16,12 +20,59 @@ public class Controller {
     private Label answerLabel;
 
     @FXML
-    private TextField userAnswer;
+    private Label textAnswer;
 
     @FXML
-    void doUserAction(ActionEvent event) {
-        Random randome = new Random();
-        String message =
-    }
+    private TextField userAnswer;
 
+
+
+   @FXML
+    void doUserAction() {
+
+       String message = userAnswer.getText();
+       userAnswer.clear();
+
+       int numberTwo = 0;
+       try {
+           numberTwo = Integer.parseInt(message);
+           for (int i = 0; i < maxCount; i++) {
+               count++;
+               if (numberTwo == multiple) {
+                   textAnswer.setText(String.valueOf(winer));
+                   break;
+
+               } else if (numberTwo >= maxCount) {
+                   var alertTwo = new  Alert(Alert.AlertType.CONFIRMATION, "Число больше ответа");
+                   textAnswer.setText(String.valueOf("К сожалению, Вы проиграли"));
+
+               } else if (numberTwo < multiple) {
+
+//                           System.out.println("Число меньше ответа");
+
+               } else if (numberTwo > multiple) {
+
+//                           System.out.println("Число больше ответа");
+               }
+
+               textAnswer.setText(String.valueOf(messageCount + (maxCount - count) + " попыток"));
+           }
+       } catch (NumberFormatException e) {
+           e.printStackTrace();
+           var alert = new Alert(Alert.AlertType.ERROR, "Введите число!");
+       }
+
+       if (!message.isBlank()) {
+               try {
+                   int number = Integer.parseInt(message);
+                   answerLabel.setText(String.valueOf(multiple));
+               } catch (NumberFormatException e) {
+                   e.printStackTrace();
+                   var alert = new Alert(Alert.AlertType.ERROR, "Введите число!");
+
+                   alert.setTitle("Ошибка ввода данных");
+                   alert.show();
+               }
+           }
+       }
 }
