4b67e0b3fd20377f96697c5d045559c48dea64b4
Alexander <tehalexpetrov@gmail.com>
2020-12-22 00:12:53 +0300

    javaFX простое приложение готово

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -2,15 +2,14 @@ package sample;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.*;
-import java.util.*;
 
 public class Controller {
 
-    public int maxCount = 3;
-    public int count = 0;
-    public int multiple = (int) Math.round(Math.random() * 9);
-    private String winer = "Поздравляю! Вы угадали число";
-    private String messageCount = "У Вас осталось ";
+    private static final int MAX_RANGE = 9;
+    private static final int MAX_COUNT = 3;
+    public int multiple = (int) Math.round(Math.random() * MAX_RANGE);
+    int count = 0;
+
 
     public void initialize(){
 
@@ -30,42 +29,44 @@ public class Controller {
    @FXML
     void doUserAction() {
 
-       String message = userAnswer.getText();
-       userAnswer.clear();
 
-       int numberTwo = 0;
-       try {
-           numberTwo = Integer.parseInt(message);
-           for (int i = 0; i < maxCount; i++) {
-               count++;
-               if (numberTwo == multiple) {
-                   textAnswer.setText(String.valueOf(winer));
-                   break;
-
-               } else if (numberTwo >= maxCount) {
-                   var alertTwo = new  Alert(Alert.AlertType.CONFIRMATION, "Число больше ответа");
-                   textAnswer.setText(String.valueOf("К сожалению, Вы проиграли"));
-
-               } else if (numberTwo < multiple) {
-
-//                           System.out.println("Число меньше ответа");
-
-               } else if (numberTwo > multiple) {
-
-//                           System.out.println("Число больше ответа");
-               }
-
-               textAnswer.setText(String.valueOf(messageCount + (maxCount - count) + " попыток"));
-           }
-       } catch (NumberFormatException e) {
-           e.printStackTrace();
-           var alert = new Alert(Alert.AlertType.ERROR, "Введите число!");
-       }
-
-       if (!message.isBlank()) {
                try {
+                   String message = userAnswer.getText();
+                   userAnswer.clear();
+
                    int number = Integer.parseInt(message);
-                   answerLabel.setText(String.valueOf(multiple));
+
+                    do {
+                        if (number == multiple) {
+                            var alert = new Alert(Alert.AlertType.INFORMATION, "Поздравляю с победой");
+                            alert.setTitle("ВЫ ВЫЙГРАЛИ!");
+                            alert.show();
+                            break;
+
+                        } else if (number > MAX_RANGE) {
+                            var alert = new Alert(Alert.AlertType.WARNING, "Введите числа от 0 до " + MAX_RANGE);
+                            alert.setTitle("Превышение диапазона чисел");
+                            alert.show();
+                            continue;
+
+                        } else if (number > multiple) {
+
+                            var alert = new Alert(Alert.AlertType.INFORMATION, "Число больше ответа");
+                            alert.setTitle("Информация");
+                            alert.show();
+                            continue;
+
+                        } else if (number < multiple) {
+
+                            var alert = new Alert(Alert.AlertType.INFORMATION, "Число меньше ответа");
+                            alert.setTitle("Информация");
+                            alert.show();
+                            continue;
+
+                        }
+
+                    } while (number < 0 && number > MAX_RANGE);
+
                } catch (NumberFormatException e) {
                    e.printStackTrace();
                    var alert = new Alert(Alert.AlertType.ERROR, "Введите число!");
@@ -73,6 +74,27 @@ public class Controller {
                    alert.setTitle("Ошибка ввода данных");
                    alert.show();
                }
-           }
-       }
+   }
+
+    @FXML
+    private Label textAnswerCheat;
+
+    @FXML
+    void cheatAnswer() {
+        maxCountTry();
+        textAnswerCheat.setText(String.valueOf(multiple));
+
+    }
+
+    @FXML
+    void closeButton() {
+        System.exit(0);
+    }
+
+    public static void maxCountTry(){
+
+                var alert = new Alert(Alert.AlertType.WARNING, "Читер ты!");
+                alert.setTitle("Информация");
+                alert.show();
+    }
 }
